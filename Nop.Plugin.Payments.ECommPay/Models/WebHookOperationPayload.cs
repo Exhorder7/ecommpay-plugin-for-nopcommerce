387cc54fb438af7500afccdb87196de1a4d10e38
holydk <omigoneo@gmail.com>
2021-08-03 14:34:44 +0300

    Add plugin

---
+++ Nop.Plugin.Payments.ECommPay/Models/WebHookOperationPayload.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+
+namespace Nop.Plugin.Payments.ECommPay.Models
+{
+    /// <summary>
+    /// Represents a operation payload of the web hook request
+    /// </summary>
+    public class WebHookOperationPayload
+    {
+        #region Properties
+
+        /// <summary>
+        /// Gets or sets the type.
+        /// </summary>
+        [JsonProperty("type")]
+        public string Type { get; set; }
+
+        #endregion
+    }
+}
